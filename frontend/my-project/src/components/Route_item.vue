da2a3764fbba35e29c342fce3b1d55df020b24c4
keeganasmith2003 <99233684+keeganasmith@users.noreply.github.com>
2024-02-24 16:18:27 -0600

    routes component

---
+++ frontend/my-project/src/components/Route_item.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        route: Object,
+        selected: Boolean
+    }
+
+})
+</script>
+
+<template>
+    <li :style="{ color: selected ? 'blue' : 'black' }" @click="handleClick">
+        {{ route["safety"] }}
+    </li>
+</template>
--- frontend/my-project/src/components/Routes_page.vue
+++ frontend/my-project/src/components/Routes_page.vue
@@ -1,12 +1,23 @@
 <template>
-
+    <h1>this is the page that will show the other map w/ possible routes</h1>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import { GoogleMap, Marker } from "vue3-google-map";
-export default{
-    
+export default defineComponent({
+    components: { GoogleMap, Marker },
+    props: { routes : Array},
+    setup() {
+        //const center = { lat: 40.689247, lng: -74.044502 };
+        const my_key = process.env.VUE_APP_GOOGLE_MAPS_API_KEY
+        return { my_key };
+    },
+    data() {
+        return {
+            selected_route: 0,
+        }
+    }
 
-}
+})
 </script>
