8ac2c7ca10ef46654af1a5a2076f4b8819dff150
brendanehrman1 <brendan.ehrman.1@tamu.edu>
2024-02-25 02:36:57 -0600

    Add cleaning script

---
+++ backend/process_data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import json
+
+df = pd.read_csv("NHTSACar.csv", encoding="unicode_escape")
+
+feature_columns = ["Size_Class","Curb_Wgt","ABS","Airbag_D","SSF","Drive","Drive4","TireSize",
+                  "StabilityControl","BrakeAssist","TractionControl","AdjUpperBeltFront","AdjUpperBeltRear",
+                  "Pretensioner","IntegratedSeat","RearCtrLapShldrBelt","AdvanceAirbagFeature","SideAirbag",
+                  "HeadAirbag","HeadAirbagRollover","RearSeatHeadRestraint","DynamicHeadRestraint","BuiltInChildSeat",
+                  "Roll_Stability","SafetyPowerWindows","WheelsDriven"]
+
+df_features = df[feature_columns]
+
+df_features['BuiltInChildSeat'] = df_features['BuiltInChildSeat'].fillna('A')
+df_features['Roll_Stability'] = df_features['Roll_Stability'].fillna('A')
+df_features['StabilityControl'] = df_features['StabilityControl'].fillna('A')
+df_features['TractionControl'] = df_features['TractionControl'].fillna('A')
+df_features['AdjUpperBeltFront'] = df_features['AdjUpperBeltFront'].fillna('A')
+df_features['Pretensioner'] = df_features['Pretensioner'].fillna('A')
+df_features['RearCtrLapShldrBelt'] = df_features['RearCtrLapShldrBelt'].fillna('A')
+df_features['AdvanceAirbagFeature'] = df_features['AdvanceAirbagFeature'].fillna('A')
+df_features['IntegratedSeat'] = df_features['IntegratedSeat'].fillna('A')
+df_features['DynamicHeadRestraint'] = df_features['DynamicHeadRestraint'].fillna('A')
+df_features['RearSeatHeadRestraint'] = df_features['RearSeatHeadRestraint'].fillna('A')
+df_features['AdjUpperBeltRear'] = df_features['AdjUpperBeltRear'].fillna('A')
+df_features['BrakeAssist'] = df_features['BrakeAssist'].fillna('A')
+df_features['HeadAirbagRollover'] = df_features['HeadAirbagRollover'].equals('Yes')
+df_features['ABS'] = df_features['ABS'].fillna('N/A')
+df_features['Airbag_D'] = df_features['Airbag_D'].fillna('N/A')
+df_features['HeadAirbag'] = df_features['HeadAirbag'].fillna('N/A')
+df_features['SideAirbag'] = df_features['SideAirbag'].fillna('N/A')
+df_features['SafetyPowerWindows'] = df_features['SafetyPowerWindows'].fillna('N/A')
+df_features['Drive'] = df_features['Drive'].combine_first(df_features['Drive4'])
+df_features.drop('Drive4', axis=1, inplace=True)
+df_features['Curb_Wgt'] = df_features['Curb_Wgt'].apply(pd.to_numeric, errors='coerce')
+df_features['SSF'] = df_features['SSF'].apply(pd.to_numeric, errors='coerce')
+
+df_features.dropna().reset_index().to_csv('cleaned_features.csv')
